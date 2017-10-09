48e840cfd4f5c30d4e6d3c7e6ea5703c73e98479
Nathaniel Woodbury <nxtezzz@gmail.com>
2017-10-09 22:24:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
-var name = "Joe"
-var height = 74
-var message = `${Nate}is ${69} inches tall`
+var name = "Nate"
+var height = 69
+var message = `${{name} }is ${height} inches tall`
 
 module.exports = {name, height, message}
 console.log(message)
